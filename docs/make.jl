8b1bf72c2767b48a15fdf02dbd95f8d7b3f30ac1
Jonathan Jalbert <jonathan.jalbert@polymtl.ca>
2021-10-16 10:08:50 -0400

    Add functions section in docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,18 @@
 using Documenter, RatingCurves
 
-makedocs(sitename="RatingCurves.jl")
+CI = get(ENV, "CI", nothing) == "true"
+
+makedocs(sitename = "RatingCurves.jl",
+    format = Documenter.HTML(
+    prettyurls = CI,
+    ),
+    pages = [
+       "index.md",
+       "contributing.md",
+       "functions.md"
+       ]
+
+)
 
 deploydocs(
     repo = "github.com/JuliaExtremes/RatingCurves.jl.git",
