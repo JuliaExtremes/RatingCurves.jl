e15d9bbeac4f42106e863c46e911a11b7897e261
Jonathan Jalbert <jonathan.jalbert@polymtl.ca>
2021-10-16 11:43:02 -0400

    Deploy dev docs

--- docs/make.jl
+++ docs/make.jl
@@ -14,6 +14,17 @@ makedocs(sitename = "RatingCurves.jl",
 
 )
 
-deploydocs(
-    repo = "github.com/JuliaExtremes/RatingCurves.jl.git",
-)
+if CI
+    deploydocs(
+    repo   = "github.com/JuliaExtremes/RatingCurves.jl.git",
+    devbranch = "dev",
+    versions = ["stable" => "v^", "v#.#"],
+    push_preview = false,
+    target = "build"
+    )
+end
+
+
+# deploydocs(
+#     repo = "github.com/JuliaExtremes/RatingCurves.jl.git",
+# )
